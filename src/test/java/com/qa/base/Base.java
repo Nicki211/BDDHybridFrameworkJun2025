d9bb11100741061e996a1182cb7b9969a67a1293
Nicki211 <nsandhale002@gmail.com>
2025-09-19 17:25:50 +0530

    Added Base class with common methods to launch and close the browser

---
+++ src/test/java/com/qa/base/Base.java
@@ -0,0 +1,41 @@
+package com.qa.base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.qa.util.ReadProperties;
+
+/**
+ * 
+ * @author Dell This Class have common methods to start and close the browser
+ *
+ */
+public class Base {
+
+
+	public static WebDriver driver;
+
+	
+	/**
+	 * @return the driver object after launching the application URL
+	 */
+	public WebDriver initializeWebDriver() {
+
+		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(ReadProperties.implicitWaitTime(), TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(ReadProperties.getappURL());
+		return driver;
+
+	}
+	
+	/**
+	 * This method will close the browser session
+	 */
+	public void closeBrowser() {
+		driver.quit();
+	}
+}
