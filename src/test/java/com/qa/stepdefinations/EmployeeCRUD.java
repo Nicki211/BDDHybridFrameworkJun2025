eaa0faf390aaa3da1f8d8474ea865cf3b77f6c18
Nicki211 <nsandhale002@gmail.com>
2025-09-27 06:36:09 +0530

    Added EmployeeCRUD changes

--- src/test/java/com/qa/stepdefinations/EmployeeCRUD.java
+++ src/test/java/com/qa/stepdefinations/EmployeeCRUD.java
@@ -73,6 +73,7 @@ public class EmployeeCRUD extends Base {
 	 */
 	@When("^I Add employee with  first name as \"([^\"]*)\" and mname as \"([^\"]*)\" and lName as \"([^\"]*)\"$")
 	public void i_Add_employee_with_first_name_as_and_mname_as_and_lName_as(String fName, String mName, String lName) throws Throwable {
+		
 		scenario.write("Adding new Employee");
 		objAddEmployeePage.addnewEmployee(fName, mName, lName);
 		WaitMethods.staticWait(5000);
@@ -81,48 +82,77 @@ public class EmployeeCRUD extends Base {
 	}
 
 	/**
-	 * @param arg1
-	 * @param arg2
-	 * @param arg3
+	 * @param fName
+	 * @param mName
+	 * @param lName
 	 * @throws Throwable
+	 * This method will search and verify newly added employee details using values fetched from feature file  
 	 */
 	@Then("^I  verify employeeAdded in list with  first name as \"([^\"]*)\" and mname as \"([^\"]*)\" and lName as \"([^\"]*)\"$")
-	public void i_verify_employeeAdded_in_list_with_first_name_as_and_mname_as_and_lName_as(String arg1, String arg2, String arg3) throws Throwable {
+	public void i_verify_employeeAdded_in_list_with_first_name_as_and_mname_as_and_lName_as(String fName, String mName, String lName) throws Throwable {
 	   
-	    
+		scenario.write(" Searching and verifying employee is added ");
+		objAddEmployeePage.navigateToEmpListPage();
+		Assert.assertEquals(fName + " " +mName, objAddEmployeePage.searchEmp(fName, mName));
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 	}
 
+	
 	/**
-	 * @param arg1
+	 * @param editEmpDataTable
 	 * @throws Throwable
+	 * This method will update employee details with the values fetched from feature file and save it
 	 */
 	@When("^I click on Edit button and update below values and save the Data$")
-	public void i_click_on_Edit_button_and_update_below_values_and_save_the_Data(DataTable arg1) throws Throwable {
+	public void i_click_on_Edit_button_and_update_below_values_and_save_the_Data(DataTable editEmpDataTable) throws Throwable {
 	    
+		scenario.write(" Editing the emp information ");
+		objAddEmployeePage.editEmp(editEmpDataTable.raw().get(0).get(1), editEmpDataTable.raw().get(1).get(1),
+				editEmpDataTable.raw().get(2).get(1));
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 	    
 	}
 
+	
 	/**
-	 * @param arg1
+	 * @param editedEmpTablevalue
 	 * @throws Throwable
+	 *  This method will search and verify newly added employee details using values fetched from feature file
 	 */
 	@Then("^I search the employee and ensure that it is searched using below values$")
-	public void i_search_the_employee_and_ensure_that_it_is_searched_using_below_values(DataTable arg1) throws Throwable {
+	public void i_search_the_employee_and_ensure_that_it_is_searched_using_below_values(DataTable editedEmpTablevalue) throws Throwable {
 	    
+		scenario.write(" Searching and verifying employee is updated ");
+
+		objAddEmployeePage.navigateToEmpListPage();
+		Assert.assertEquals(editedEmpTablevalue.raw().get(0).get(1) + " " +editedEmpTablevalue.raw().get(1).get(1),
+				objAddEmployeePage.searchEmp(editedEmpTablevalue.raw().get(0).get(1),
+						editedEmpTablevalue.raw().get(1).get(1)));
+
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 	    
 	}
 
 	/**
 	 * @throws Throwable
+	 * This method will search deleted emp deatils
 	 */
 	@Then("^I select and Delete the Updated Employee and verify employee is not  in search result$")
 	public void i_select_and_Delete_the_Updated_Employee_and_verify_employee_is_not_in_search_result() throws Throwable {
 	    
-	    
+		scenario.write(" Deleting Updated Record  ");
+
+		Assert.assertEquals("No Records Found", objAddEmployeePage.deleteUdpateEmp());
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 	}
 
 	/**
-	 * @param scenario After hook to close the app
+	 * @param scenario 
+	 * After hook to close the app
 	 */
 	@After
 
